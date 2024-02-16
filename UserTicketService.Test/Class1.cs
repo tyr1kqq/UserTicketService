ed530034f2f0e99d6f9aadda3ccbddc976a99239
tyr1kqq <igorturkinwhite@yandex.ru>
2024-02-16 15:09:06 +0200

    Добавьте файлы проекта.

---
+++ UserTicketService.Test/Class1.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace UserTicketService.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [SetUp]
+
+        public void Setup()
+        {
+
+        }
+       
+        [Test]
+        public void Test1()
+        {
+            ClassicAssert.True(100 == 100);
+        }
+    }
+}
---
+++ UserTicketService/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace UserTicketService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
